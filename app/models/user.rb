e025cdc3f2ed0f1956c3678b57c963da01dbea08
Nate Hill <natanio@gmail.com>
2015-06-30 17:01:54 +0900

    create learner after new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,17 @@
 class User < ActiveRecord::Base
   enum role: [:user, :coach, :admin]
   after_initialize :set_default_role, :if => :new_record?
+  after_create :create_learner
 
   has_many :coaches
   has_many :learners
 
   def set_default_role
     self.role ||= :user
+  end
+
+  def create_learner
+    self.learners.create
   end
 
   # Include default devise modules. Others available are:
