985fcc3232a2071ad66f5ac93dd3e88d23a13a3e
Nate Hill <natanio@gmail.com>
2015-07-03 15:26:59 +0900

    add learner profile form

--- app/controllers/learners_controller.rb
+++ app/controllers/learners_controller.rb
@@ -1,4 +1,6 @@
-class LearnerssController < UsersController
+class LearnersController < UsersController
+  before_action :authenticate_user!
+  after_action :verify_authorized
 
   def index
   end
@@ -10,8 +12,21 @@ class LearnerssController < UsersController
   end
 
   def update
+    @learner = Learner.find(params[:id])
+    authorize @learner
+    if @learner.update_attributes(secure_params)
+      redirect_to :back, :notice => "Your Learner settings have been updated."
+    else
+      redirect_to :back, :alert => "Unable to update your learner settings."
+    end
   end
 
   def destroy
+  end
+
+private
+
+  def secure_params
+    params.require(:learner).permit(:bio, :reason_why)
   end
 end
