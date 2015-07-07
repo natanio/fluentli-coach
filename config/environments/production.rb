bd8815db93d01246560591df66d33699137a8bf7
Nate Hill <natanio@gmail.com>
2015-07-07 11:51:07 +0900

    trying again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,14 +78,11 @@ Rails.application.configure do
 
   # For paperclip and AWS
   config.paperclip_defaults = {
-    :s3_host_name => 's3-us-west-2.amazonaws.com',
-    :storage => :s3,
-    :url => :s3_domain_url,
-    :path => "/:user/:id/:style/:filename",
-    :s3_credentials => {
-      :bucket => ENV['AWS_BUCKET'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['S3_BUCKET_NAME'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   }
+}
 end
---
+++ config/initializers/paperclip.rb
@@ -0,0 +1,6 @@
+# set s3 path
+Paperclip::Attachment.default_options[:url] = ':s3_domain_url'
+Paperclip::Attachment.default_options[:path] = '/:user/:avatar/:id/:style/:filename'
+
+# set s3 server
+Paperclip::Attachment.default_options[:s3_host_name] = 's3-us-west-2.amazonaws.com'
